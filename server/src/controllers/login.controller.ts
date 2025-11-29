6219e4da5ace9fccc217406a6f65be25d44247d1
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2025-11-28 20:54:13 -0800

    done

--- server/src/controllers/login.controller.ts
+++ server/src/controllers/login.controller.ts
@@ -16,6 +16,7 @@ export const login = async (req: Request, res: Response) => {
     }
 
     //createToken
+    // token done
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.TOKEN_SECRET!,
