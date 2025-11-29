b46d4222ddec34824b298b9a50f8c293fec75254
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2025-11-29 23:06:15 +0800

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./config/connect.db";
-
+import cors from "cors";
 import userRoute from "./routers/user.router";
 import roleRouter from "./routers/role.router";
 dotenv.config();
+
 connectDb();
+
 const app = express();
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use("/api/auth", userRoute);
 app.use("/api/users", roleRouter);
 
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 5001;
+console.log("BACKEND STARTED");
 app.get("/", (req, res) => {
-  res.send("started back-end hello world");
+  console.log("REQUEST RECEIVED");
+  res.send("OK WORKING");
 });
-
-app.listen(port as number, "localhost", () => {
+app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
