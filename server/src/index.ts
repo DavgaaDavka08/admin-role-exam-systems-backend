c3ebcd7e0b8fb071270d00d02199ffcad0e5b647
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2025-11-30 20:30:47 +0800

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,28 +4,38 @@ import { connectDb } from "./config/connect.db";
 import cors from "cors";
 import userRoute from "./routers/user.router";
 import roleRouter from "./routers/role.router";
-dotenv.config();
 
+dotenv.config();
 connectDb();
 
 const app = express();
-
 app.use(express.json());
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://admin-role-front-end.vercel.app",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
+
+app.options("*", cors());
+
 app.use("/api/auth", userRoute);
 app.use("/api/users", roleRouter);
 
 const port = process.env.PORT || 4000;
+
 console.log("BACKEND STARTED");
+
 app.get("/", (req, res) => {
   console.log("REQUEST RECEIVED");
   res.send("OK WORKING");
 });
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
